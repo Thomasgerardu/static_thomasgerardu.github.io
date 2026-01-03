aad38ead63179978461f8fc4e5eac0c6ad8f8e99
TG <thomasgerardu@hotmail.com>
2026-01-03 23:43:11 +0100

    chips bijgewerkt.

--- processor.py
+++ processor.py
@@ -84,4 +84,4 @@ csv_processor = ChipsCSVProcessor('chips.csv')
 csv_processor.rename_files()
 
 # Call replace_placeholder_dates function
-csv_processor.replace_placeholder_dates('2025/06/11','2025/11/03')
+csv_processor.replace_placeholder_dates('2025/11/03','2026/01/03')
